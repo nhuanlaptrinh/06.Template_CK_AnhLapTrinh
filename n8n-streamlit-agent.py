76075301fc1a5e9fe40fc73e26f3ab328ef9c224
Quoc Dat <quocdattranhuu1606@gmail.com>
2025-06-03 13:06:44 +0700

    update code

--- n8n-streamlit-agent.py
+++ n8n-streamlit-agent.py
@@ -14,6 +14,10 @@ def rfile(name_file):
     except FileNotFoundError:
         st.error(f"File {name_file} không tồn tại.")
 
+# Khởi tạo tin nhắn "system" và "assistant"
+INITIAL_SYSTEM_MESSAGE = {"role": "system", "content": rfile("01.system_trainning.txt")}
+INITIAL_ASSISTANT_MESSAGE = {"role": "assistant", "content": rfile("02.assistant.txt")}
+
 # Constants
 WEBHOOK_URL = rfile("WEBHOOK_URL.txt").strip()
 SUPABASE_URL = rfile("SUPABASE_URL.txt").strip()
@@ -268,8 +272,8 @@ def main():
             f"""<h1 style="text-align: center; font-size: 24px; margin-bottom: 20px;">{title_content}</h1>""",
             unsafe_allow_html=True
         )
-        st.markdown(f'''<div class="message-container"><div class="assistant" style=" ">Em Nhi ở đây để tư vấn về "Trợ Lý A.I".
-Sếp inbox nội dung cần tư vấn giúp em Nhi nhé !</div></div>''', unsafe_allow_html=True)
+        st.markdown(f'<div class="message-container"><div class="assistant">{INITIAL_ASSISTANT_MESSAGE["content"]}</div></div>', unsafe_allow_html=True)
+
         if st.sidebar.button("Đăng xuất", key="logout_button"):
             handle_logout()
 
